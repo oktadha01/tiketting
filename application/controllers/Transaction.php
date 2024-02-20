ef0cfa2ebac1d19a47c10d0337726d33aed819e1
oktadha01 <oktadha01@gmail.com>
2024-02-20 14:10:54 +0700

    transaksi

--- application/controllers/Transaction.php
+++ application/controllers/Transaction.php
@@ -72,36 +72,7 @@ class Transaction extends CI_Controller
                         $tgl_transaksi = '<span class="smal">Dipesan</span>
                                             <span class="font-weight-bold">' . $data_transaksi->tgl_transaksi . '</span>';
                         // coundown
-                        $scriptcountdown = '<script>
-                            var tgl_transaksi = "02-20-2024 14:18:52";
-                    
-                            var end = new Date(tgl_transaksi);
-                            var _second = 1000;
-                            var _minute = _second * 60;
-                            var _hour = _minute * 60;
-                            var _day = _hour * 24;
-                            var timer;
-                    
-                            function showRemaining() {
-                                var now = new Date();
-                                var distance = end - now;
-                                var days = Math.floor(distance / _day);
-                                var hours = Math.floor((distance % _day) / _hour);
-                                var minutes = Math.floor((distance % _hour) / _minute);
-                                var seconds = Math.floor((distance % _minute) / _second);
-                    
-                                document.getElementById("demo-' . $data_transaksi->id_transaksi . '").innerHTML = "00:" + minutes + ":" + seconds;
-                    
-                                if (distance < 0) {
-                                    clearInterval(timer);
-                                    document.getElementById("demo-' . $data_transaksi->id_transaksi . '").innerHTML = "EXPIRED!";
-                                    return;
-                                }
-                            }
-                            timer = setInterval(showRemaining, 1000);
-                    
-                    </script>';
-                        $countdown = '<div id="demo-' . $data_transaksi->id_transaksi . '" class="mb-0"></div>';
+                       
                     } elseif ($data_transaksi->status_transaksi == '1') {
                         // Status transaksi
                         $status_transaksi = '<td class="font-weight-medium"><div class="badge badge-info shadow l-green text-dark rounded">Lunas</div></td>';
@@ -111,7 +82,7 @@ class Transaction extends CI_Controller
                                         </div>';
                         // Tanggal transaksi
                         $tgl_transaksi = '<span class="smal">Dibayar</span>
-                                        <span class="font-weight-bold">' . $data_transaksi->tgl_bayar . '</span>';
+                                        <span class="font-weight-bold">' . $data_transaksi->tgl_byr . '</span>';
                     }
 
                     echo '<div class="card box-shadow">
@@ -163,12 +134,7 @@ class Transaction extends CI_Controller
             echo '<span>Tidak Ada Transaksi</span>';
         }
     }
-    // <div class="row">
-    //                         <div class="col-12">
-    //                         ' . $countdown . '
-    //                         ' . $scriptcountdown . '
-    //                         </div>
-    //                     </div>
+   
     function detail_trans()
     {
         $code_bayar = $_POST['code_bayar'];
--- code.php
+++ code.php
@@ -57,3 +57,39 @@
 
     });
 </script>
+
+<div class="row">
+    <div class="col-12">
+        ' . $countdown . '
+        ' . $scriptcountdown . '
+    </div>
+</div>
+$scriptcountdown = '<script>
+    var tgl_transaksi = "02-20-2024 14:18:52";
+
+    var end = new Date(tgl_transaksi);
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+    var timer;
+
+    function showRemaining() {
+        var now = new Date();
+        var distance = end - now;
+        var days = Math.floor(distance / _day);
+        var hours = Math.floor((distance % _day) / _hour);
+        var minutes = Math.floor((distance % _hour) / _minute);
+        var seconds = Math.floor((distance % _minute) / _second);
+
+        document.getElementById("demo-' . $data_transaksi->id_transaksi . '").innerHTML = "00:" + minutes + ":" + seconds;
+
+        if (distance < 0) {
+            clearInterval(timer);
+            document.getElementById("demo-' . $data_transaksi->id_transaksi . '").innerHTML = "EXPIRED!";
+            return;
+        }
+    }
+    timer = setInterval(showRemaining, 1000);
+</script>';
+$countdown = '<div id="demo-' . $data_transaksi->id_transaksi . '" class="mb-0"></div>';
